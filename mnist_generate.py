2e14d8035fb88ecf153955ccf0321ae31b9f1153
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2020-05-07 21:29:17 +0200

    a

--- mnist_generate.py
+++ mnist_generate.py
@@ -50,7 +50,6 @@ noise1 = torch.cat((z, c1, c2), dim=1)
 noise2 = torch.cat((z, c1, c3), dim=1)
 
 from google.colab.patches import cv2_imshow
-from PIL import Image
 
 # Generate image.
 with torch.no_grad():
@@ -58,7 +57,8 @@ with torch.no_grad():
 # Display the generated image.
 fig = plt.figure(figsize=(10, 10))
 plt.axis("off")
-plt.imshow(Image.fromarray(np.transpose(vutils.make_grid(generated_img1, nrow=10, padding=2, normalize=True), (1,2,0))))
+type(np.transpose(vutils.make_grid(generated_img1, nrow=10, padding=2, normalize=True), (1,2,0)))
+plt.imshow(np.transpose(vutils.make_grid(generated_img1, nrow=10, padding=2, normalize=True), (1,2,0)))
 plt.show()
 cv2_imshow(np.transpose(vutils.make_grid(generated_img1, nrow=10, padding=2, normalize=True), (1,2,0)))
 plt.show()
