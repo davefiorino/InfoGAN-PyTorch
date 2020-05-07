20e431e74b78443ef3eae79ce87f2da8eb37f91f
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2020-05-07 21:16:43 +0200

    fix imshow

--- mnist_generate.py
+++ mnist_generate.py
@@ -49,16 +49,14 @@ noise1 = torch.cat((z, c1, c2), dim=1)
 # To see variation along c3 (Horizontally) and c1 (Vertically)
 noise2 = torch.cat((z, c1, c3), dim=1)
 
-from google.colab.patches import cv2_imshow
-
 # Generate image.
 with torch.no_grad():
     generated_img1 = netG(noise1).detach().cpu()
 # Display the generated image.
+%matplotlib inline
 fig = plt.figure(figsize=(10, 10))
 plt.axis("off")
-#plt.imshow(np.transpose(vutils.make_grid(generated_img1, nrow=10, padding=2, normalize=True), (1,2,0)))
-cv2_imshow(np.transpose(vutils.make_grid(generated_img1, nrow=10, padding=2, normalize=True), (1,2,0)))
+plt.imshow(np.transpose(vutils.make_grid(generated_img1, nrow=10, padding=2, normalize=True), (1,2,0)))
 plt.show()
 
 # Generate image.
@@ -67,6 +65,5 @@ with torch.no_grad():
 # Display the generated image.
 fig = plt.figure(figsize=(10, 10))
 plt.axis("off")
-#plt.imshow(np.transpose(vutils.make_grid(generated_img2, nrow=10, padding=2, normalize=True), (1,2,0)))
-cv2_imshow(np.transpose(vutils.make_grid(generated_img2, nrow=10, padding=2, normalize=True), (1,2,0)))
+plt.imshow(np.transpose(vutils.make_grid(generated_img2, nrow=10, padding=2, normalize=True), (1,2,0)))
 plt.show()
