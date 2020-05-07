36b18604122269c2f7cd318264b516eb5e5ff32c
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2020-05-07 23:25:35 +0200

    a

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ params = {
     'batch_size': 128,# Batch size.
     'num_epochs': 10,# Number of epochs to train for. 
     'learning_rate': 2e-4,# Learning rate.
-    'beta1': 0.5,
-    'beta2': 0.999,
+    'beta1': 0.5, # Momentum parameter
+    'beta2': 0.999, #RMSprop parameter
     'save_epoch' : 10,# After how many epochs to save checkpoints and generate test output.
-    'dataset' : 'MNIST'}# Dataset to use. Choose from {MNIST, SVHN, CelebA, FashionMNIST}. CASE MUST MATCH EXACTLY!!!!!
+    'dataset' : 'MNIST'# Dataset to use. Choose from {MNIST, SVHN, CelebA, FashionMNIST}. Case sensitive!
+}
