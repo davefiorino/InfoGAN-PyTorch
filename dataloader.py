348ce2d8105ae85ced23db3a0b5e78dc961e52d4
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2020-05-09 22:01:10 +0200

    a

--- dataloader.py
+++ dataloader.py
@@ -59,7 +59,8 @@ def get_data(dataset, batch_size):
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5], std=[0.5])])
 
-        dataset = dsets.ImageFolder(root=root+'chest_xray', transform=transform)
+        dataset = dsets.ImageFolder(root=root+'chest_xray/train', transform=transform)
+        print(dataset.__len__)
 
     # Create dataloader.
     dataloader = torch.utils.data.DataLoader(dataset, 
