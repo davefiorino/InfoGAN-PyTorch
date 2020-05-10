422176928b834cbb0caa9dd9dc92d9cdfb9f8092
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2020-05-10 14:15:39 +0200

    a

--- models/chestxray_model.py
+++ models/chestxray_model.py
@@ -9,8 +9,8 @@ Architecture by Davide Fiorino.
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
-        # 168 x 1 x 1
-        self.tconv1 = nn.ConvTranspose2d(168, 1024, 1, 1, bias=False)
+        # 146 x 1 x 1
+        self.tconv1 = nn.ConvTranspose2d(146, 1024, 1, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(1024)
         # 1024 x 1 x 1
         self.tconv2 = nn.ConvTranspose2d(1024, 128, kernel_size=7, stride=1, bias=False)
--- train.py
+++ train.py
@@ -67,10 +67,10 @@ elif(params['dataset'] == 'FashionMNIST'):
     params['num_con_c'] = 2
 elif(params['dataset'] == 'ChestXRay'):
     params['num_z'] = 124
-    params['num_dis_c'] = 4
+    params['num_dis_c'] = 2
     params['dis_c_dim'] = 10
-    params['num_con_c'] = 4
-    
+    params['num_con_c'] = 2
+
 # Plot the training images.
 sample_batch = next(iter(dataloader))
 plt.figure(figsize=(10, 10))
