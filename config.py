44aa1234bd33d840c7ca38b23919891b5cc6b4db
Ubuntu <ubuntu@ip-172-31-45-81.eu-central-1.compute.internal>
2020-05-12 15:39:09 +0000

    a

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Dictionary storing network parameters.
 params = {
-    'batch_size': 64, # Batch size.
-    'num_epochs': 50, # Number of epochs to train for. 
-    'learning_rate': 2e-4, # Learning rate. (2e-4)
+    'batch_size': 128, # Batch size.
+    'num_epochs': 20, # Number of epochs to train for. 
+    'learning_rate': 2e-3, # Learning rate. (2e-4)
     'beta1': 0.5, # Momentum parameter
     'beta2': 0.999, # RMSprop parameter
     'save_epoch' : 20, # After how many epochs to save checkpoints and generate test output.
