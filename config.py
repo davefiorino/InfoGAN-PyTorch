3b125d6e33c4adb0dfd25d3f4c1f7f65b1eb6ab4
Ubuntu <ubuntu@ip-172-31-45-81.eu-central-1.compute.internal>
2020-05-12 16:04:06 +0000

    a

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 params = {
     'batch_size': 64, # Batch size.
     'num_epochs': 20, # Number of epochs to train for. 
-    'learning_rate': 5e-4, # Learning rate. (2e-4)
+    'learning_rate': 7e-4, # Learning rate. (2e-4)
     'beta1': 0.5, # Momentum parameter
     'beta2': 0.999, # RMSprop parameter
     'save_epoch' : 20, # After how many epochs to save checkpoints and generate test output.
