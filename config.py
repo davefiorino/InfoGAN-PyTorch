5dcba2594221a90d17abc00507ba78ef3134031d
Ubuntu <ubuntu@ip-172-31-45-81.eu-central-1.compute.internal>
2020-05-13 15:26:01 +0000

    a

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ params = {
     'learning_rate_G': 2e-4,# learning rate for generator 
     'beta1': 0.5, # Momentum parameter
     'beta2': 0.999, # RMSprop parameter
-    'save_epoch' : 20, # After how many epochs to save checkpoints and generate test output.
+    'save_epoch' : 10, # After how many epochs to save checkpoints
     'dataset' : 'ChestXRay', # Choose from {MNIST, SVHN, CelebA, FashionMNIST, ChestXRay}
     'print_model_description' : True, # Print the description of the models during training and generation
-    'image_log': False # Save a generated image at every epoch (if False every 10 epochs)
+    'image_log_epoch': 10 # Save a generated image every X epochs
 }
