2cf06c5ff1dd080476f757369c9d210c5a4bc45f
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2020-05-13 23:23:06 +0200

    a

--- models/chestxray_model.py
+++ models/chestxray_model.py
@@ -167,9 +167,9 @@ class QHead(nn.Module):
         self.conv1 = nn.Conv2d(1024, 128, 4, bias=False)
         self.bn1 = nn.BatchNorm2d(128)
 
-        self.conv_disc = nn.Conv2d(128, 20, 1)
-        self.conv_mu = nn.Conv2d(128, 2, 1)
-        self.conv_var = nn.Conv2d(128, 2, 1)
+        self.conv_disc = nn.Conv2d(128, 10, 1)
+        self.conv_mu = nn.Conv2d(128, 4, 1)
+        self.conv_var = nn.Conv2d(128, 4, 1)
 
     def forward(self, x):
         x = F.leaky_relu(self.bn1(self.conv1(x)), 0.1, inplace=True)
