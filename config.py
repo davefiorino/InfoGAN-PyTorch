1f00a7c26177d7a208d7006908fefe16ecb26713
Ubuntu <ubuntu@ip-172-31-45-81.eu-central-1.compute.internal>
2020-05-14 11:13:47 +0000

    a

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Dictionary storing network parameters.
 params = {
-    'batch_size': 64, # Batch size.
-    'num_epochs': 100, # Number of epochs to train for. 
+    'batch_size': 128, # Batch size.
+    'num_epochs': 500, # Number of epochs to train for. 
     'learning_rate_D': 2e-4, # Learning rate for discriminator
     'learning_rate_G': 2e-4,# learning rate for generator 
     'beta1': 0.5, # Momentum parameter
@@ -9,5 +9,5 @@ params = {
     'save_epoch' : 10, # After how many epochs to save checkpoints
     'dataset' : 'ChestXRay', # Choose from {MNIST, SVHN, CelebA, FashionMNIST, ChestXRay}
     'print_model_description' : True, # Print the description of the models during training and generation
-    'image_log_epoch': 5 # Save a generated image every X epochs
+    'image_log_epoch': 1 # Save a generated image every X epochs
 }
