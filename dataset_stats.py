e482e0bf5c8fc1fc39a3ffbe260262ade15d9c1d
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2020-05-23 14:41:32 +0200

    a

---
+++ dataset_stats.py
@@ -0,0 +1,48 @@
+# Compute mean and std of the dataset
+# ChestXRay: mean=0.5711, std=0.1774
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+
+dataset = dsets.ImageFolder(root='data/chest_xray/train', transform=transforms.Compose([transforms.Resize(256),
+                             transforms.CenterCrop(256),
+                             transforms.ToTensor()]))
+
+loader = torch.utils.data.DataLoader(dataset,
+                        batch_size=128,
+                         num_workers=0,
+                         shuffle=False)
+
+# mean = 0.0
+# for images, _ in loader:
+#     batch_samples = images.size(0) 
+#     images = images.view(batch_samples, images.size(1), -1)
+#     mean += images.mean(2).sum(0)
+# mean = mean / len(loader.dataset)
+# print(mean)
+
+# var = 0.0
+# for images, _ in loader:
+#     batch_samples = images.size(0)
+#     images = images.view(batch_samples, images.size(1), -1)
+#     var += ((images - mean.unsqueeze(1))**2).sum([0,2])
+# std = torch.sqrt(var / (len(loader.dataset)*256*256))
+# print(std)
+
+
+import tensorflow as tf
+from PIL import ImageStat
+
+class Stats(ImageStat.Stat):
+    def __add__(self, other):
+        return Stats(list(map(add, self.h, other.h)))
+
+statistics = None
+for data in loader:
+    for b in range(data.shape[0]):
+        if statistics is None:
+            statistics = Stats(tf.to_pil_image(data[b]))
+        else:
+            statistics += Stats(tf.to_pil_image(data[b]))
+
+print(f'mean:{statistics.mean}, std:{statistics.stddev}')
