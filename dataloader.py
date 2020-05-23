44a9c90571523d6b5baa4d95cde3598bac9d9690
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2020-05-23 13:00:37 +0200

    a

--- dataloader.py
+++ dataloader.py
@@ -60,8 +60,8 @@ def get_data(dataset, batch_size):
             transforms.Resize(256),
             transforms.CenterCrop(256),
             transforms.Grayscale(num_output_channels=1),
-            transforms.ToTensor()])
-            #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]])
+            transforms.ToTensor(),
+            transforms.Normalize(mean=0.5711, std=0.1774)])
 
         dataset = dsets.ImageFolder(root=root+'chest_xray/train', transform=transform)
         print('Dataset len:', len(dataset))
